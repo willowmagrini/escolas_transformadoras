0937687a1aae5faf0743a9fbed8b153b08d0d442
Bruno Magrini <brmagrini@gmail.com>
2015-11-05 19:30:48 -0200

    acerta p do equipe

--- content-equipe.php
+++ content-equipe.php
@@ -2,16 +2,22 @@
 	<?php
 	 	?>
 		<div>
+			<a data-id="<?php echo $post->ID;?>" href="<?php the_permalink()?>">
+			
 		<?php 
 			echo get_the_post_thumbnail( $post->ID, 'thumbnail' ); 
 		?>
+		</a>
+		
 		</div>
 		<a data-id="<?php echo $post->ID;?>" href="<?php the_permalink()?>">
 			<?php echo get_the_title();
 			?>
 		</a>
+		<p>
 		<?php 
 			echo the_excerpt();
 		?>
+		</p>
 	
 </div>
