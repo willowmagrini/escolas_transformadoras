41036d7a5aeba5686006792ecdd55175585f021e
Bruno Magrini <brmagrini@gmail.com>
2016-01-01 16:35:43 -0200

    comeca materiais novo

--- single-material.php
+++ single-material.php
@@ -7,16 +7,8 @@
  */
 
 get_header(); ?>
-
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.5";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
 	<main id="content" class="pagina <?php echo odin_classes_page_full(); ?>" tabindex="-1" role="main">
+
 			<?php
 				// Start the Loop.
 				while ( have_posts() ) : the_post();
@@ -27,6 +19,11 @@ get_header(); ?>
 					 * (where ___ is the post format) and that will be used instead.
 					 */
 					get_template_part( 'content', get_post_format() );
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
 				endwhile;
 			?>
 			<div class='paginacao'>
@@ -38,16 +35,6 @@ get_header(); ?>
 					<?php next_post_link('%link', __('Próximo ','odin').'<div class="seta-pag "></div>'); ?> 
 				</div>
 			</div>
-			<?php
-		 		$url= esc_url( get_permalink() ); 
-				$path= parse_url($url, PHP_URL_PATH);
-				$path = explode('/',parse_url($url, PHP_URL_PATH));
-				$lang=$path[1];
-				$url = str_replace("/".$lang."/","/", $url);
-				
-				
-			?>
-			<div class="fb-comments" data-href="<?php echo $url?>" data-width="100%" data-numposts="5"></div>
 		</main><!-- #main -->
 	
 		 
